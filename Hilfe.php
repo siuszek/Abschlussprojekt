6ecbd95783f50e820ce95365c81ff987f1d5127a
siuszek <67790094+siuszek@users.noreply.github.com>
2020-07-03 15:55:10 +0200

    Add files via upload

---
+++ Hilfe.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Hilfe</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    </head>
+    <body>
+
+
+        <div class="container">
+            <h1 class="adsimple-121317788">Wie funktioniert das?</h1>
+            <p class="adsimple-121317788">Hier erfährst du, wie die Website funktioniert</p>
+            <p class="adsimple-121317788">
+                <strong>For any additional questions feel free to Contact Us:</strong> <br><br>
+                <strong>E-Mail:</strong> <a href="mailto:office@sutterhealth.com">office@sutterhealth.com</a> <br><br>
+                <strong>Telephone Number:</strong> (925) 584-4439<br />
+                <strong>Office Hours:</strong> Monday - Friday (8am-6pm)<br /> <br>
+                <strong>Fax Number:</strong> 44444/55555<br />
+            </p>
+            <div>
+                <img src="src/traveller_world.jfif" alt="logo" height = 500 width = 700>
+            </div>
+        </div>
+    </body>
+</html>
